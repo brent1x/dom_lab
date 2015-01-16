fb59d3067acc2075a9e9509db93c8d82c66345a1
Brent Dady <brentdady@gmail.com>
2015-01-15 16:45:09 -0800

    first commit, prob 1

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// Your code here
+// 1
+
+window.onload = function() {
+  swapGreeting();
+};
+
+function swapGreeting() {
+  var swapper = document.getElementById("greeting").innerHTML = "Hello, Planet Earth!";
+  console.log("logMeBro // ", swapper);
+}
+
+
+// 2
+
+
+// 3
+// click handler - add class of 'selected'
+
+
+// 4
+
+
+// 5
+
+
+
