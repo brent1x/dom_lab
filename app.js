7d64eb6f25cee8dc449db875e67ae726ad281b87
Brent Dady <brentdady@gmail.com>
2015-01-16 10:35:08 -0800

    last commit

--- app.js
+++ app.js
@@ -1,26 +1,52 @@
-// 1
-
-window.onload = function() {
-  swapGreeting();
-};
-
-function swapGreeting() {
-  var swapper = document.getElementById("greeting").innerHTML = "Hello, Planet Earth!";
-  console.log("logMeBro // ", swapper);
+// 1 – SWAP GREETING
+function a() {
+	var swapGreeting = document.getElementById("greeting").innerHTML = "Hello, Planet Earth!";
+	console.log("test1", swapGreeting);
 }
 
+// 2 – CHANGE LI BACKGROUND TO YELLOW
+function b() {
+	var swapList = document.getElementsByTagName("li");
+	for (i = 0; i < swapList.length; i++) {
+		swapList[i].style.backgroundColor = "yellow";
+	}
+	console.log("test2", swapList);
+}
 
-// 2
+// 3 – ON CLICK, ADD LI ITEMS TO SELECTED CLASS (STRIKE THROUGH IN HTML)
+// 4 – LAST CLICKED ITEM SHOULD SWAP IMAGE TO LAST CLICKED IMAGE
+function cd() {
+	var liItems = document.getElementsByTagName("li");
+	for (i = 0; i < liItems.length; i++) {
+		liItems[i].addEventListener("click", clickClack);	
+	}
+	console.log("test3", liItems);	
+}
+	
+function clickClack(event) {
+		// SWAP LI CLASS TO SELECTED
+		event.target.className = "selected";
+		console.log("test4");
+		// SWAP IMAGES TO LAST CLASS SELECTED
+		document.querySelector("img").setAttribute("src", "./images/"+event.target.innerHTML+".jpeg");
+		console.log("test5");
+}
 
+// 5 – RESET BUTTON REMOVES SELECTED CLASS, CHANGES IMAGES TO PANICKED BRO
+function e() {
+	var lulz = document.getElementsByTagName("li");
+		for (i = 0; i < lulz.length; i++) {
+			lulz[i].className = "reset";
+		}
+	document.querySelector("img").setAttribute("src", "./images/panic.jpeg");
+	console.log("test6");
+}
 
-// 3
-// click handler - add class of 'selected'
+var initialize = function () {
+	a();
+	b();
+	cd();
+	document.getElementById("reset").addEventListener("click", e);
+};
 
-
-// 4
-
-
-// 5
-
-
-
+window.onload = initialize;
